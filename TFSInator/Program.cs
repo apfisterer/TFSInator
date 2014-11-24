caa895a26de7223e6d106d54b7c7722225361343
Adrian Pfisterer <adrian@pfamily.org>
2014-11-23 23:18:35 -0700

    First commit

---
+++ TFSInator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using TFSInator.Properties;
+
+namespace TFSInator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TFSInatorContext());
+        }
+    }
+
+    public class TFSInatorContext : ApplicationContext
+    {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        
+        private NotifyIcon trayIcon;
+
+        public TFSInatorContext()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.Icon1,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("About", About),
+                    new MenuItem("Exit", Exit)
+            }),
+                Visible = true, 
+            };
+
+            trayIcon.Text = "TFSInator";
+            var HotKeyManager = new HotkeyManager();
+
+            string letter = ConfigurationManager.AppSettings["hotKeyLetter"];
+            char c = letter[0];
+            int cInt = System.Convert.ToInt16(c);
+            if(cInt < 65 || cInt > 90)
+            {
+                string s = string.Format("Illegal value in App.Config for hotKeyLetter {0}. Must be A - Z", letter);
+                throw new ApplicationException(s);
+            }
+
+            RegisterHotKey(HotKeyManager.Handle, 123, Constants.CTRL + Constants.ALT, cInt);
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        void About(object sender, EventArgs e)
+        {
+            new AboutBox().Show();
+        }
+
+    }
+}
---
+++ TFSInator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TFSInator")]
+[assembly: AssemblyDescription("This utility will load the TFS work item in the user's clipboard in the user's default browser.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TFSInator")]
+[assembly: AssemblyCopyright("Copyright Adrian Pfisterer ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a7075fe-636a-4349-b842-46c4e4afad82")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
